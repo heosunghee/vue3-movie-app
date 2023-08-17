0c28a5ecf6cd494fc4bff313c72a494d63e7fb6d
sunghee <tjdgml211018@naver.com>
2023-08-17 14:46:42 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+// 개발자정보, 사용자 정보
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'SUNGHEE',
+    email: 'tjdgml211018@naver.com',
+    blog: 'https://github.com/uzen-stella',
+    phone: '+82-10-1234-5678',
+    image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDAzMDhfMjYz%2FMDAxNTgzNjAyODEyMzcw.x91qWu-8M3D3uMhLCibFLegQj3_H2jrhXHIxyeJvHdgg.u9s8UBAqjQPyedrot4xWLgKRogd-2YaHEj-lBozRVgog.PNG.dana_7%2FprofileImg013x.png&type=sc960_832'
+  })
+}
