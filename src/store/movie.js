4fb58ffca6a6ea1bd264fd535a0b2ee7212f1116
sunghee <tjdgml211018@naver.com>
2023-08-18 13:04:11 +0900

    Add Netlify functions

--- src/store/movie.js
+++ src/store/movie.js
@@ -60,7 +60,7 @@ export default {
         console.log(typeof totalResults) //string
 
         const total = parseInt(totalResults, 10)
-        const pageLength = Math.ceil(total / 10)
+        const pageLength = Math.ceil(total / 10) // 총 페이지 길이
 
         // 추가 요청
         if (pageLength > 1) {
@@ -119,21 +119,6 @@ export default {
   }
 }
 
-function _fetchMovie(payload) {
-  const { title, type, year, page, id } = payload
-  const OMDB_API_KEY = '7035c60c'
-  const url = id ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-
-  return new Promise((resolve, reject) => {
-    axios.get(url)
-      .then(res => {
-        if (res.data.Error) {
-          reject(res.data.Error)
-        }
-        resolve(res)
-      })
-      .catch(err => {
-        reject(err.message)
-      })
-  })
+async function _fetchMovie(payload) {
+  return await axios.post('/.netlify/functions/movie', payload)
 }
